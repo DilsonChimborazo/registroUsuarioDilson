6819ce7f7dab354554bff1054c8f715a57940cae
dilsonchimborazo <dilsonchimborazoperez@gmail.com>
2024-08-16 11:06:07 -0500

    agragando las vistas

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Persona
+from .form import personaform
 
 # Create your views here.
+
+def registrouser(request):
+    if request.method == 'POST':
+        form = personaform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarUsuario')
+    else:
+        form = personaform()
+    return render(request,
+    template_name='/registroUsuario.html',context={'form':
+    form})
